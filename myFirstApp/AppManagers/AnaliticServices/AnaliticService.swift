c1c70a61ddc823b35fcda4cdf23c8150b407ef09
mikitka <nikuvar77@gmail.com>
2026-09-20 04:54:32 +0300

    rename scheme

--- myFirstApp/AppManagers/AnaliticServices/AnaliticService.swift
+++ myFirstApp/AppManagers/AnaliticServices/AnaliticService.swift
@@ -16,7 +16,7 @@ class AnalyticService {
 
     private init() {}
     
-    let environment: Environment = .dev
+    let environment: Environment = .prod
     
     func logEvent(name: String, properties: [AnyHashable : Any]) {
         if isTrackingAuthorized == nil {
